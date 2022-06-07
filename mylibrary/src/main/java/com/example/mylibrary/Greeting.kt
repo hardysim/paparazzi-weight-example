04d906d3c5427171fe54c170b641d2fe605208e5
Simon Hardt <s.hardt@appdev.de>
2022-06-07 15:29:10 +0200

    add composable using width and verticalScroll

--- mylibrary/src/main/java/com/example/mylibrary/Greeting.kt
+++ mylibrary/src/main/java/com/example/mylibrary/Greeting.kt
@@ -1,19 +1,66 @@
 package com.example.mylibrary
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mylibrary.theme.MyApplicationTheme
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Card(
+        modifier = Modifier.padding(24.dp),
+        elevation = 4.dp,
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column(modifier = Modifier
+                .weight(weight = 1f, fill = false)
+            ) {
+                Text(
+                    style = MaterialTheme.typography.h5,
+                    text = "Headline Text",
+                )
+
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                        .weight(weight = 1f, fill = false),
+                ) {
+                    Text("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.End,
+            ) {
+                Button(onClick = { }) {
+                    Text("click me")
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
